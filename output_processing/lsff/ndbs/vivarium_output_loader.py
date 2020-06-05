7090e84734fb811ea30cd8af4113323068c0442c
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-06-05 13:45:12 -0700

    add new more modular functions to load data; write wrapper function to get mean, upper, lower

--- output_processing/lsff/ndbs/vivarium_output_loader.py
+++ output_processing/lsff/ndbs/vivarium_output_loader.py
@@ -1,6 +1,69 @@
 import pandas as pd
 import os
 
+def locaction_paths_from_rundates(base_directory: str, locations_rundates: dict) -> dict:
+    """
+    Returns a dictionary of the form {'location': 'pathname'} from a dictionary of the form
+    {'location': 'rundate'} by constructing each pathname as
+    pathname = f'{base_directory}/{location.lower()}/{rundate}/'
+    """
+    return {'location': os.path.join(base_directory, location.lower(), rundate)
+            for location, rundate in locations_rundates.items()}
+
+def load_output_by_location(locations_paths: dict, output_filename='output.hdf') -> pd.DataFrame:
+    """
+    Loads output.hdf files for the locations in the dictionary locations_paths into a single,
+    concatenated DataFrame, with a 'location' column added.
+    """
+    # Read in data from different countries
+    locations_outputs = {location: pd.read_hdf(os.path.join(path, output_filename))
+                                               for location, path in locactions_paths.items()}
+    
+    for location, output in locations_outputs.items():
+        output['location'] = location
+
+    return pd.concat(locations_outputs.values(), copy=False, sort=False)
+
+def load_transformed_count_data(directory: str) -> dict:
+    """
+    Loads each transformed "count space" .hdf output file into a dataframe,
+    and returns a dictionary whose keys are the file names and values are
+    are the corresponding dataframes.
+    """
+    dfs = {}
+    for entry in os.scandir(directory):
+        filename_root, extension = os.path.splitext(entry.name)
+        if extension == '.hdf':
+#             print(filename_root, type(filename_root), extension, entry.path)
+            dfs[filename_root] = pd.read_hdf(entry.path)
+    return dfs
+
+def load_count_data_by_location(locations_paths: dict, subdirectory='count_data') -> dict:
+    """
+    Loads data from all locations into a dictionary of dataframes,
+    indexed by filename, with a column added to each table specifying
+    the location.
+    
+    For each location, reads data files from a directory called f'{path}/{subdirectory}/',
+    where `path` is the path for the location specified in the `locations_paths` dictionary.
+    """
+    location_dictionaries = []
+    for location, path in locations_paths.items():
+        location_dfs = load_transformed_count_data(os.path.join(path, subdirectory))
+        for table in location_dfs.values():
+            # Insert the 'location' column at the beginning
+            table.insert(0, 'location', location)
+        
+        location_dictionaries.append(location_dfs)
+        
+    data_dict = location_dictionaries[0]
+   
+    for location_dfs in location_dictionaries[1:]:
+        for table_name, table in location_dfs.items():
+            data_dict[table_name] = data_dict[table_name].append(table)
+            
+    return data_dict
+
 def load_by_location_and_rundate(base_directory: str, locations_run_dates: dict) -> pd.DataFrame:
     """Load output.hdf files from folders namedd with the convention 'base_directory/location/rundate/output.hdf'"""
 
@@ -23,20 +86,6 @@ def print_location_output_shapes(all_output, locations=None):
         locations = all_output.location.unique()
     for location in locations:
         print(location, all_output.loc[all_output.location==location].shape)
-
-def load_transformed_count_data(directory):
-    """
-    Loads each transformed "count space" .hdf output file into a dataframe,
-    and returns a dictionary whose keys are the file names and values are
-    are the corresponding dataframes.
-    """
-    dfs = {}
-    for entry in os.scandir(directory):
-        filename_root, extension = os.path.splitext(entry.name)
-        if extension == '.hdf':
-#             print(filename_root, type(filename_root), extension, entry.path)
-            dfs[filename_root] = pd.read_hdf(entry.path)
-    return dfs
 
 def path_for_transformed_count_data(directory, location, rundate, subdirectory='count_data'):
     """
