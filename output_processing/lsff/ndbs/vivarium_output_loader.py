db23d2ee2ffb9bbbd3d37764cde0436d69507d51
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-09-24 16:09:33 -0700

    split output_loader functions into two steps; add notebook

--- output_processing/lsff/ndbs/vivarium_output_loader.py
+++ output_processing/lsff/ndbs/vivarium_output_loader.py
@@ -7,22 +7,54 @@ def locaction_paths_from_rundates(base_directory: str, locations_rundates: dict)
     {'location': 'rundate'} by constructing each pathname as
     pathname = f'{base_directory}/{location.lower()}/{rundate}/'
     """
-    return {'location': os.path.join(base_directory, location.lower(), rundate)
+    return {location: os.path.join(base_directory, location.lower(), rundate)
             for location, rundate in locations_rundates.items()}
 
-def load_output_by_location(locations_paths: dict, output_filename='output.hdf') -> pd.DataFrame:
+def load_output_by_location(locations_paths: dict, output_filename='output.hdf') -> dict:
     """
-    Loads output.hdf files for the locations in the dictionary locations_paths into a single,
-    concatenated DataFrame, with a 'location' column added.
+    Loads output.hdf file from the path for each location in the dictionary locations_paths,
+    and returns a dictionary of output DataFrames indexed by location.
     """
     # Read in data from different countries
     locations_outputs = {location: pd.read_hdf(os.path.join(path, output_filename))
                                                for location, path in locations_paths.items()}
+    return locations_outputs
+
+def merge_location_outputs(locations_outputs: dict) -> pd.DataFrame:
+    """
+    Concatenate the output DataFrames for all locations stored in locations_outputs into a single DataFrame,
+    with a 'location' column added to specify which location each row came from.
     
+    This function modifies the input data...
+    """
     for location, output in locations_outputs.items():
         output['location'] = location
 
     return pd.concat(locations_outputs.values(), copy=False, sort=False)
+
+def load_and_merge_location_outputs(locations_paths: dict, output_filename='output.hdf') -> pd.DataFrame:
+    """
+    Loads output.hdf files from the paths for the locations in the dictionary locations_paths into a single,
+    concatenated DataFrame, with a 'location' column added to specify which location each row came from.
+    """
+    locations_outputs = load_output_by_location(locations_paths, output_filename)
+    return merge_location_outputs(locations_outputs)
+
+# # Old version, replaced with above functions on 2020-09-24:
+#
+# def load_output_by_location(locations_paths: dict, output_filename='output.hdf') -> pd.DataFrame:
+#     """
+#     Loads output.hdf files for the locations in the dictionary locations_paths into a single,
+#     concatenated DataFrame, with a 'location' column added.
+#     """
+#     # Read in data from different countries
+#     locations_outputs = {location: pd.read_hdf(os.path.join(path, output_filename))
+#                                                for location, path in locations_paths.items()}
+    
+#     for location, output in locations_outputs.items():
+#         output['location'] = location
+
+#     return pd.concat(locations_outputs.values(), copy=False, sort=False)
 
 def load_transformed_count_data(directory: str) -> dict:
     """
@@ -40,29 +72,68 @@ def load_transformed_count_data(directory: str) -> dict:
 
 def load_count_data_by_location(locations_paths: dict, subdirectory='count_data') -> dict:
     """
-    Loads data from all locations into a dictionary of dataframes,
-    indexed by filename, with a column added to each table specifying
-    the location.
+    Loads data from all locations into a dictionary of dictionaries of dataframes,
+    indexed by location. Each dictionary in the outer dictionary is
+    indexed by filename
     
     For each location, reads data files from a directory called f'{path}/{subdirectory}/',
     where `path` is the path for the location specified in the `locations_paths` dictionary.
     """
-    location_dictionaries = []
-    for location, path in locations_paths.items():
-        location_dfs = load_transformed_count_data(os.path.join(path, subdirectory))
-        for table in location_dfs.values():
-            # Insert the 'location' column at the beginning
+    locations_count_data = {location: load_transformed_count_data(os.path.join(path, subdirectory))
+                        for location, path in locations_paths.items()}
+    return locations_count_data
+
+def merge_location_count_data(locations_count_data: dict) -> dict:
+    """
+    This function modifies the input data...
+    """
+    for location, data in locations_count_data.items():
+        for table in data.values():
+            # Insert a 'location' column at the beginning of each table
             table.insert(0, 'location', location)
         
-        location_dictionaries.append(location_dfs)
+    # `data` now refers to the dictionary of count_data tables for the last location
+    # encountred in the above for loop. We will use the keys stored in this dictionary
+    # to iterate through all the table names and concatenate the tables across all locations.
+    all_data = {table_name: pd.concat([locations_count_data[location][table_name]
+                                      for location in locations_count_data], copy=False, sort=False)
+                for table_name in data}
+    return all_data
+
+def load_and_merge_location_count_data(locations_paths: dict, subdirectory='count_data') -> dict:
+    locations_count_data = load_count_data_by_location(locations_paths, subdirectory)
+    return merge_location_count_data(locations_count_data)
+
+# # Old version, replaced with above functions on 2020-09-24:
+#
+# def load_count_data_by_location(locations_paths: dict, subdirectory='count_data') -> dict:
+#     """
+#     Loads data from all locations into a dictionary of dataframes,
+#     indexed by filename, with a column added to each table specifying
+#     the location.
+    
+#     For each location, reads data files from a directory called f'{path}/{subdirectory}/',
+#     where `path` is the path for the location specified in the `locations_paths` dictionary.
+#     """
+#     location_dictionaries = []
+#     for location, path in locations_paths.items():
+#         location_dfs = load_transformed_count_data(os.path.join(path, subdirectory))
+#         for table in location_dfs.values():
+#             # Insert the 'location' column at the beginning
+#             table.insert(0, 'location', location)
         
-    data_dict = location_dictionaries[0]
+#         location_dictionaries.append(location_dfs)
+        
+#     data_dict = location_dictionaries[0]
    
-    for location_dfs in location_dictionaries[1:]:
-        for table_name, table in location_dfs.items():
-            data_dict[table_name] = data_dict[table_name].append(table)
+#     for location_dfs in location_dictionaries[1:]:
+#         for table_name, table in location_dfs.items():
+#             data_dict[table_name] = data_dict[table_name].append(table)
             
-    return data_dict
+#     return data_dict
+
+##### 2020-09-24: The following functions are old and should be superceded by the above versions,
+##### which do a better job of separating concerns.
 
 def load_by_location_and_rundate(base_directory: str, locations_run_dates: dict) -> pd.DataFrame:
     """Load output.hdf files from folders namedd with the convention 'base_directory/location/rundate/output.hdf'"""
