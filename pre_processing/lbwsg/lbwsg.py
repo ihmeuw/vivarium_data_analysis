6ffcee604f0b62e47087abd79ef7dfed53fbcee4
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-11-20 21:46:27 -0800

    add functions to load LBWSG data from hdf stores

---
+++ pre_processing/lbwsg/lbwsg.py
@@ -0,0 +1,67 @@
+"""
+This file will probably be a lightweight version of some of the things from the
+LBWSG component in Vivarium Public Health:
+
+https://github.com/ihmeuw/vivarium_public_health/blob/master/src/vivarium_public_health/risks/implementations/low_birth_weight_and_short_gestation.py
+"""
+
+import pandas as pd
+import re
+from typing import Tuple#, Dict, Iterable
+import gbd_mapping
+
+def read_lbwsg_data_by_draw(artifact_path, measure, draw, rename=None):
+    """
+    Reads one draw of LBWSG data from an artifact.
+    
+    measure should be one of:
+    'exposure'
+    'relative_risk'
+    'population_attributable_fraction'
+    rename should be a string or None (default)
+    """
+    key = f'risk_factor/low_birth_weight_and_short_gestation/{measure}'
+    with pd.HDFStore(artifact_path, mode='r') as store:
+        index = store.get(f'{key}/index')
+        draw = store.get(f'{key}/draw_{draw}')
+    if rename is not None:
+        draw.rename(rename)
+    data = pd.concat([index, draw], axis=1)
+    return data
+
+def read_lbwsg_data1(artifact_path, measure, *filter_terms, draws='all'):
+    query_string = ' and '.join(filter_terms)
+    if draws=='all':
+        draws = range(1000)
+    draw_cols = [f'draw_{draw}' for draw in draws]
+    draw_data_dfs = []
+    
+    for draw in draws:
+        draw_data = read_lbwsg_data_by_draw(artifact_path, measure, draw)
+        if query_string != '':
+            draw_data = draw_data.query(query_string)
+        index_cols = draw_data.columns.difference(draw_cols)
+        draw_data = draw_data.set_index(index_cols.to_list())
+#         data = pd.concat([data, draw_data], axis=1)
+        draw_data_dfs.append(draw_data)
+        
+#     return data
+    return pd.concat(draw_data_dfs, axis=1, copy=False)
+
+def read_lbwsg_data(artifact_path, measure, *filter_terms, draws='all'):
+    key = f'risk_factor/low_birth_weight_and_short_gestation/{measure}'
+    query_string = ' and '.join(filter_terms)
+    if draws=='all':
+        draws = range(1000)
+    
+    with pd.HDFStore(artifact_path, mode='r') as store:
+        index_cols = store.get(f'{key}/index')
+        if query_string != '':
+            index_cols = index_cols.query(query_string)
+        draw_data_dfs = [index_cols]
+        for draw in draws:
+            draw_data = store.get(f'{key}/draw_{draw}') # draw_data is a pd.Series
+            draw_data = draw_data[index_cols.index] # filter to query on index columns
+            draw_data_dfs.append(draw_data)
+
+    return pd.concat(draw_data_dfs, axis=1, copy=False).set_index(index_cols.columns.to_list())
