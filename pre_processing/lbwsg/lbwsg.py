248d2039902d786a5c0eea39a0630a1e9c7c900d
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-11-21 00:28:32 -0800

    update category-interval conversion functions; start writing a class for lbwsg distributtion

--- pre_processing/lbwsg/lbwsg.py
+++ pre_processing/lbwsg/lbwsg.py
@@ -8,7 +8,12 @@ https://github.com/ihmeuw/vivarium_public_health/blob/master/src/vivarium_public
 import pandas as pd
 import re
 from typing import Tuple#, Dict, Iterable
-import gbd_mapping
+import gbd_mapping as gbd
+
+# The support of the LBWSG distribution is nonconvex, but adding this one category makes it convex,
+# which makes life easier when shifting the birthweights or gestational ages.
+# I think the category number was just arbitrarily chosen from those that weren't already taken.
+MISSING_CATEGORY = {'cat212': 'Birth prevalence - [37, 38) wks, [1000, 1500) g'}
 
 def read_lbwsg_data_by_draw(artifact_path, measure, draw, rename=None):
     """
@@ -49,6 +54,9 @@ def read_lbwsg_data1(artifact_path, measure, *filter_terms, draws='all'):
     return pd.concat(draw_data_dfs, axis=1, copy=False)
 
 def read_lbwsg_data(artifact_path, measure, *filter_terms, draws='all'):
+    """
+    Reads multiple draws from the artifact.
+    """
     key = f'risk_factor/low_birth_weight_and_short_gestation/{measure}'
     query_string = ' and '.join(filter_terms)
     if draws=='all':
@@ -76,13 +84,14 @@ def get_intervals_from_name(name: str) -> Tuple[pd.Interval, pd.Interval]:
     return (pd.Interval(numbers_only[0], numbers_only[1], closed='left'),
             pd.Interval(numbers_only[2], numbers_only[3], closed='left'))
 
-def get_lbwsg_categories_by_interval(category_dict):
+def get_lbwsg_categories_by_interval(include_missing=True):
     """
     Return a pandas Series indexed by (gestational age interval, birth weight interval),
     mapping to the corresponding LBWSG category.
     """
-    MISSING_CATEGORY = 'cat212'
-    category_dict[MISSING_CATEGORY] = 'Birth prevalence - [37, 38) wks, [1000, 1500) g'
+    category_dict = gbd.risk_factors.low_birth_weight_and_short_gestation.categories.to_dict()
+    if include_missing:
+        category_dict.update(MISSING_CATEGORY)
     cats = (pd.DataFrame.from_dict(category_dict, orient='index')
             .reset_index()
             .rename(columns={'index': 'cat', 0: 'name'}))
@@ -91,4 +100,72 @@ def get_lbwsg_categories_by_interval(category_dict):
     cats = cats['cat']
     cats.index = idx
     return cats
+
+def get_ga_bw_by_category(include_missing=True):
+    """
+    Returns a dataframe indexed by LBWSG category, with four columns describing the intervals for that category:
+    ga_start, ga_end, bw_start, bw_end
+    """
+    cats = gbd.risk_factors.low_birth_weight_and_short_gestation.categories.to_dict()
+    if include_missing:
+        cats.update(MISSING_CATEGORY)
+    cats = pd.Series(cats)
+    #Example string to extract from: 'Birth prevalence - [0, 24) wks, [0, 500) g'
+    return cats.str.extract(r'Birth prevalence - \[(?P<ga_start>\d+), (?P<ga_end>\d+)\) wks, \[(?P<bw_start>\d+), (?P<bw_end>\d+)\) g')
+#     return cats.str.extract(r'.*\[(?P<ga_start>\d+), (?P<ga_end>\d+).*\[(?P<bw_start>\d+), (?P<bw_end>\d+).*')
+
+class LBWSGDistribution():
+    """
+    Class to assign and adjust birthweights and gestational ages of a simulated population.
+    """
+    def __init__(self):
+        self.cat_df = get_ga_bw_by_category()
+        self.cat_df['ga_width'] = self.cat_df['ga_end'] - self.cat_df['ga_start']
+        self.cat_df['bw_width'] = self.cat_df['bw_end'] - self.cat_df['bw_start']
+    
+    def cat_to_ga_bw(cat, p1, p2):
+        """Map from category `cat` to bivariate continuous values of birth weight and gestational age.
+
+        Example usage:
+
+        cat_df = get_ga_bw_by_category()
+        cat = np.random.choice(cat_df.index, size=None)
+        cat, cat_to_ga_bw(cat, np.random.rand(), np.random.rand())
+
+        Parameters
+        ----------
+        cat : str or list-like of strings, one of the 58 values in index of cat_df
+        p1 : float or list-like of floats, propensity between 0 and 1
+        p2 : float or list-like of floats, propensity between 0 and 1
+
+        Results
+        -------
+        Returns tuple of (gestational age, birth weight)
+        """
+
+        ga = self.cat_df.loc[cat, 'ga_start'] + p1 * self.cat_df.loc[cat, 'ga_width']
+        bw = self.cat_df.loc[cat, 'bw_start'] + p2 * self.cat_df.loc[cat, 'bw_width']
+
+        return ga, bw
+    
+    def ga_bw_to_cat(self, ga, bw):
+        """Map from (birth weight, gestational age) to category name.
+
+        Example usage:
+
+        ga_bw_to_cat(31., 3298.)
+
+        Parameters
+        ----------
+        ga : float (gestational age)
+        bw : float (birth weight)
+
+        Results
+        -------
+        Returns cat for (bw,ga) pair
+        """
+
+        t = self.cat_df.query('@bw >= bw_start and @bw < bw_end and @ga >= ga_start and @ga < ga_end')
+        assert len(t) >= 1
+        return t.index[0]
 
