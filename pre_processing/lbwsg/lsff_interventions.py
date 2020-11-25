b444e76041aa151d367ee5fd49575fc12acfeb2b
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-11-25 15:00:34 -0800

    start writing iron birthweight intervention

---
+++ pre_processing/lbwsg/lsff_interventions.py
@@ -0,0 +1,95 @@
+import pandas as pd, numpy as np
+from scipy import stats
+
+import sys, os.path
+sys.path.append(os.path.abspath("../.."))
+from probability import prob_utils
+from plots_and_other_misc import lsff_plots
+
+# Define distributions of iron concentration in fortified flour for each country,
+# representing parameter uncertainty.
+# Units are mg iron as NaFeEDTA per kg flour
+# Currently we use a uniform distribution for India and degenerate (point mass)
+# distributions for Ethiopia and Nigeria.
+# Eventually this should be stored in some external data source that will then be loaded.
+iron_conc_distributions = {
+    'India': stats.uniform(loc=14, scale=21.5-14), # Uniform(14,21.5) mg iron as NaFeEDTA per kg flour
+    'Ethiopia': stats.bernoulli(p=0,loc=30) # 30 mg iron as NaFeEDTA per kg flour,
+    'Nigeria': stats.bernoulli(p=0,loc=40) # 40 mg iron as NaFeEDTA per kg flour,
+}
+
+def sample_flour_consumption(sample_size):
+    """Sample from distribution of daily flour consumption (in Ethiopia).
+    The distribution is uuniform between each quartile: min=0, q1=77.5, q2=100, q3=200, max=350.5
+    This distribution represents individual heterogeneity and currently has no parameter uncertainty.
+
+    Currently, the data is hardcoded, but eventually it should be location-dependent.
+    The Ethiopia data comes from the Ethiopian National Food Consumption Survey (2013).
+    """
+    # Define quartiles in g of flour per day
+    q = (0, 77.5, 100, 200, 350.5) # min=0, q1=77.5, q2=100, q3=200, max=350.5
+    u = np.random.uniform(0,1,size=sample_size)
+    # Scale the uniform random number to the correct interval based on its quartile
+    return np.select(
+        [u<0.25, u<0.5, u<0.75, u<1],
+        [q[1]*u/0.25, q[1]+(q[2]-q[1])*(u-0.25)/0.25, q[2]+(q[3]-q[2])*(u-0.5)/0.25, q[3]+(q[4]-q[3])*(u-0.75)/0.25]
+    )
+
+def create_bw_dose_response_distribution(self):
+    """Define normal distribution representing parameter uncertainty of dose-response on birthweight.
+    mean = 15.1 g per 10 mg daily iron, 95% CI = (6.0,24.2).
+    Effect size comes from Haider et al. (2013)
+    """
+    # mean and 0.975-quantile of normal distribution for mean difference (MD)
+    mean = 15.1 # g per 10 mg daily iron
+    q_975 = 24.2 # 97.5th percentile
+    std = prob_utils.normal_stdev_from_mean_quantile(mean, q_975, 0.975)
+    # Frozen normal distribution for MD, representing uncertainty in our effect size
+    return stats.norm(mean, std)
+
+def calculate_birthweight_shift(dose_response, iron_concentration, daily_flour):
+    """
+    Computes the increase in birthweight (in grams) given the following:
+    
+    dose_response: g of birthweight increase per 10 mg daily iron
+    iron_concentration: mg iron as NaFeEDTA per kg flour
+    daily flour: g of iron eaten per day by pregnant mother
+    """
+    return (dose_response/10)*(iron_concentration)*(daily_flour/1_000)
+
+# Import dataframe storing wheat flour fortification coverage parameters
+coverage_df = lsff_plots.get_coverage_dfs()['flour'].T
+
+class IronFortificationIntervention:
+    """
+    Class for applying iron intervention to simulants.
+    """
+    propensity_name = 'iron_fortification_propensity'
+    
+    def __init__(self, location, basline_coverage_key, target_coverage_key):
+        self.iron_conc_distribution = iron_conc_distributions[location]
+        self.bw_dose_response_distribution = create_bw_dose_response_distribution()
+        self.baseline_coverage = coverage_df.loc[location, basline_coverage_key]
+        self.target_coverage = coverage_df.loc[location, target_coverage_key]
+    
+    def assign_treatment_deleted_birthweight(pop, lbwsg_distribution):
+        """
+        Assigns "treatment-deleted" birthweights to each simulant in the population.
+        """
+        flour_consumption_distribution = sample_flour_consumption(10_000)
+        # Currently these distributions are sampling one value for all draws.
+        # TODO: Update to sample a different value for each draw.
+        dose_response = self.bw_dose_response_distribution.rvs()
+        iron_conc = self.iron_conc_distribution.rvs() 
+        mean_bw_shift = calculate_birthweight_shift(dose_response, iron_conc, pop['mother_daily_flour']).mean()
+        # Shift everyone's birthweight down by the average shift
+        pop['treatment_deleted_birthweight'] = lbwsg_distribution.apply_birthweight_shift(
+            pop, - self.baseline_coverage * mean_bw_shift)
+    
+    
+    
+    
+    
+
+        
+        
