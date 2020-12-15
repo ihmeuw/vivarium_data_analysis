66b3ef73d4a2310754ab1e3aad587d0fe68c03de
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-12-14 16:33:41 -0800

    create .py file and add functions for checking model versions and pulling VAD prevalence

---
+++ pre_processing/lsff_project/pull_gbd2019_data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import sys, os.path
+sys.path.append(os.path.abspath("../.."))
+from pre_processing.id_helper import *
+import rank_countries_by_stunting as rbs
+
+from db_queries import get_ids, get_population, get_outputs, get_best_model_versions
+from get_draws.api import get_draws
+
+def find_best_model_versions(search_string, entity='modelable_entity', decomp_step='step4', **kwargs_for_contains):
+    """Searches for entity id's with names matching search_string using pandas.Series.str.contains,
+    and calls get_best_model_versions with appropriate arguments to determine if decomp_step is correct.
+    """
+    best_model_versions = get_best_model_versions(
+        entity,
+        ids = find_ids('modelable_entity', search_string, **kwargs_for_contains),
+        gbd_round_id = list_ids('gbd_round', '2019'),
+        status = 'best',
+        decomp_step = decomp_step,
+    )
+    return best_model_versions
+
+def pull_vad_prevalence_for_locations(location_ids):
+    """Calls `get_draws()` to pull vitamin A deficiency exposure for 
+    the location id's in the locations_ids iterable.
+    """
+    vad = get_draws(
+        'rei_id',
+        gbd_id=list_ids('rei', 'Vitamin A deficiency'),
+        source='exposure',
+        location_id=list(location_ids),
+        year_id=2019,
+        age_group_id=list_ids('age_group', 'Under 5'),
+        sex_id=list_ids('sex', 'Both'),
+        gbd_round_id=list_ids('gbd_round', '2019'),
+        status='best',
+        decomp_step='step4',
+    )
+    return vad
