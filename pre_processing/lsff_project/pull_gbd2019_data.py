ed3e37155a04a7e4888098db54c355ffdb19407a
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-12-15 12:19:33 -0800

    write function to pull VAD burden

--- pre_processing/lsff_project/pull_gbd2019_data.py
+++ pre_processing/lsff_project/pull_gbd2019_data.py
@@ -24,6 +24,7 @@ def pull_vad_prevalence_for_locations(location_ids):
     """Calls `get_draws()` to pull vitamin A deficiency exposure for 
     the location id's in the locations_ids iterable.
     """
+    # I got an error when I tried to pass measure_id or metric_id; the default is to return all available
     vad = get_draws(
         'rei_id',
         gbd_id=list_ids('rei', 'Vitamin A deficiency'),
@@ -37,3 +38,25 @@ def pull_vad_prevalence_for_locations(location_ids):
         decomp_step='step4',
     )
     return vad
+
+def pull_vad_daly_burden_for_locations(location_ids):
+    vad_burden = get_draws(
+        gbd_id_type=['rei_id', 'cause_id'], # Types must match gbd_id's
+        gbd_id=[list_ids('rei', 'Vitamin A deficiency'), list_ids('cause', 'All causes')],
+        source='burdenator',
+        measure_id=find_ids('measure', 'DALYs'),
+        metric_id=list_ids('metric', 'Number'), # Rate is not available, only Number and Percent
+        location_id=list(location_ids),
+        year_id=2019,
+        age_group_id=list_ids('age_group',
+                             'Early Neonatal', 'Late Neonatal', 'Post Neonatal', '1 to 4'), # Age group aggregates not available
+        sex_id=list_ids('sex', 'Male', 'Female'), # Sex aggregates not available
+        gbd_round_id=list_ids('gbd_round', '2019'),
+        status='best',
+        decomp_step='step5',
+    )
+    return vad_burden
+
+def calculate_percent_global_VAD_burden():
+    pass
+
