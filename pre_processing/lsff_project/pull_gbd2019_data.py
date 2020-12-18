970d75d54adb4852a419492a4649d922958e6a57
Nathaniel Blair-Stahn <ndbs@uw.edu>
2020-12-18 15:03:35 -0800

    fix pulling cause data; write function to get iron dalys by subpopulation; move adding global location to separate function

--- pre_processing/lsff_project/pull_gbd2019_data.py
+++ pre_processing/lsff_project/pull_gbd2019_data.py
@@ -72,6 +72,7 @@ def pull_vad_daly_burden_for_locations(location_ids):
 def pull_dalys_attributable_to_risk_for_locations(risk_factor_name, location_ids):
     """Calls get_draws to pull all-cause DALYs attributable to the specified risk for the specified locations.
     The call does not specify the age_group_id, so it will pull DALYs for all age groups that contriibute DALYs.
+    Estimates from age group aggregates are not available from the burdenator.
     """
     burden = get_draws(
         gbd_id_type=['rei_id', 'cause_id'], # Types must match gbd_id's
@@ -91,13 +92,14 @@ def pull_dalys_attributable_to_risk_for_locations(risk_factor_name, location_ids
 def pull_dalys_for_cause_for_locations(cause_name, location_ids):
     dalys = get_draws(
         gbd_id_type='cause_id',
-        gbd_id=[list_ids('rei', cause_name), list_ids('cause', cause_name)],
+        gbd_id=list_ids('cause', cause_name),
         source='dalynator',
         measure_id=find_ids('measure', 'DALYs'),
         metric_id=list_ids('metric', 'Number'),
         location_id=list(location_ids),
         year_id=2019,
         sex_id=list_ids('sex', 'Male', 'Female'),
+        age_group_id=list_ids('age_group', 'All Ages'), #22,#
         gbd_round_id=list_ids('gbd_round', '2019'),
         status='best',
         decomp_step='step5',
@@ -130,11 +132,18 @@ def calculate_proportion_global_burden(burden_for_locations, global_burden):
     # Reset the location_id level in denominator to broadcast over global instead of trying to match location
     return burden_for_locations / global_burden.reset_index('location_id', drop=True)
 
+def add_global_location(locations):
+    locations = locations[['location_name', 'location_id']]
+    global_loc = pd.Series(['Global', 1], index=locations.columns)
+    return locations.append(global_loc, ignore_index=True)
+    
+
 def calculate_all_proportion_global_burdens(locations_key):
     """Claculate"""
     locations = get_locations(locations_key)[['location_name', 'location_id']]
-    global_loc = pd.Series(['Global', 1], index=locations.columns)
-    locations = locations.append(global_loc, ignore_index=True)
+#     global_loc = pd.Series(['Global', 1], index=locations.columns)
+#     locations = locations.append(global_loc, ignore_index=True)
+    locations = add_global_location(locations)
     
     risks = ['Vitamin A deficiency', 'Zinc deficiency', 'Iron deficiency']
     risk_burdens = [pull_dalys_attributable_to_risk_for_locations(risk, locations.location_id) for risk in risks]
@@ -160,7 +169,38 @@ def summarize_risk_proportions(risk_burden_proportions):
     risk_burden_proportions['mean_lb_ub'] = risk_burden_proportions.apply(
         lambda row: f"{row['mean']:.2%} ({row['lower']:.2%}, {row['upper']:.2%})", axis=1)
     risk_burden_proportions.index = ids_to_names('rei', *risk_burden_proportions.index)
-    return risk_burden_proportions[[ 'mean_lb_ub', 'mean', 'lower', 'upper']]
+    return risk_burden_proportions[['mean_lb_ub', 'mean', 'lower', 'upper']]
+
+def get_iron_dalys_by_subpopulation(iron_burden):
+#     wra_age_groups = range(list_ids('age_group', '10 to 14'), list_ids('age_group', '55 to 59'))
+#     under5_age_groups = range(list_ids('age_group', 'Early Neonatal'), list_ids('age_group', '5 to 9'))
+#     male_id = list_ids('sex', 'Male')
+    
+#     wra_query = f"sex_id==2 and age_group_id in {wra_age_groups}"
+#     under5_query = f"age_group_id in {under5_age_groups}"
+#     # rest of population = At least 5 years old and (Male or not between ages 10 and 54)
+#     other_pop_query = f"(age_group_id not in {under5_age_groups}) and " \
+#                       f"(sex_id == {male_id} or age_group_id not in {wra_age_groups})"
+    
+    wra_query = "sex_id==2 and 7 <= age_group_id <= 15" # Female and '10 to 15' to '50 to 54'
+    under5_query = "age_group_id <= 5" # '1 to 4' age group
+    # rest of population = At least 5 years old and (Male or not between ages 10 and 54)
+    other_pop_query = "age_group_id > 5 and (sex_id == 1 or age_group_id < 7 or age_group_id > 15)"
+    
+    iron_wra = iron_burden.query(wra_query)
+    iron_under5 = iron_burden.query(under5_query)
+    iron_other = iron_burden.query(other_pop_query)
+    
+    subpops = ('WRA', 'Under 5', 'Other')
+    dfs = (iron_wra, iron_under5, iron_other)
+    totals = [aggregate_draws_over_columns(df, ['age_group_id', 'sex_id', 'location_id']) for df in dfs]
+    totals = [df.T.describe(percentiles=[0.025,0.975]) for df in totals]
+    for df in totals:
+        df.columns=[0]
+    d = {subpop: df for subpop, df in zip(subpops, totals)}
+    
+#     return iron_wra, iron_under5, iron_other
+    return {'WRA':totals[0], 'Under 5': totals[1], 'Other': totals[2]}
     
     
 
