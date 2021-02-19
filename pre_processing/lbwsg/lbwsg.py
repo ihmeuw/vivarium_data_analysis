0ebef8537f426954f871e57dd7fdec7e3e2e4867
Nathaniel Blair-Stahn <ndbs@uw.edu>
2021-02-19 00:26:06 -0800

    write function to get category descriptions using get_ids or gbd_mapping

--- pre_processing/lbwsg/lbwsg.py
+++ pre_processing/lbwsg/lbwsg.py
@@ -8,14 +8,157 @@ https://github.com/ihmeuw/vivarium_public_health/blob/master/src/vivarium_public
 import pandas as pd, numpy as np
 import re
 from typing import Tuple#, Dict, Iterable
-import gbd_mapping as gbd
+
+# import gbd_mapping as gbd
+import importlib
+if importlib.util.find_spec('gbd_mapping') is not None:
+    gbd = importlib.import_module('gbd_mapping')
+if importlib.util.find_spec('db_queries') is not None:
+    db_queries = importlib.import_module('db_queries')
 
 # The support of the LBWSG distribution is nonconvex, but adding this one category makes it convex,
 # which makes life easier when shifting the birthweights or gestational ages.
 # I think the category number was just arbitrarily chosen from those that weren't already taken.
-MISSING_CATEGORY = {'cat212': 'Birth prevalence - [37, 38) wks, [1000, 1500) g'}
+# Note: In GBD 2019, this category is `cat124`, meid=20224.
+MISSING_CATEGORY_GBD_2017 = {'cat212': 'Birth prevalence - [37, 38) wks, [1000, 1500) g'}
 
-def read_lbwsg_data_by_draw(artifact_path, measure, draw, rename=None):
+CATEGORY_MEIDS_GBD_2019 = [10755, 10761, 10763, 10764, 10767, 10768, 10770, 10772, 10773, 10774, 10775, 10776, 10777, 10778, 10779, 10780, 10781, 10782, 10783, 10784, 10785, 10786, 10787, 10788, 10789, 10790, 10791, 10792, 10793, 10794, 10795, 10796, 10797, 10798, 10799, 10800, 10801, 10802, 10803, 10804, 10805, 10806, 10807, 10808, 10809, 20203, 20204, 20205, 20209, 20210, 20211, 20214, 20215, 20221, 20224, 20227, 20228, 20232]
+
+# The dictionary below was created with the following code:
+# CATEGORY_TO_MEID_GBD_2019 = (
+#     lbwsg_exposure_nigeria_birth_male
+#      .dropna()
+#      .set_index('parameter')
+#      ['modelable_entity_id']
+#      .astype(int)
+#      .sort_index(key=lambda s: s.str.strip('cat').astype(int))
+#      .to_dict()
+#     )
+# where `lbwsg_exposure_nigeria_birth_male` was exposure data from `get_draws`.
+
+CATEGORY_TO_MEID_GBD_2019 = {
+ 'cat2': 10755,
+ 'cat8': 10761,
+ 'cat10': 10763,
+ 'cat11': 10764,
+ 'cat14': 10767,
+ 'cat15': 10768,
+ 'cat17': 10770,
+ 'cat19': 10772,
+ 'cat20': 10773,
+ 'cat21': 10774,
+ 'cat22': 10775,
+ 'cat23': 10776,
+ 'cat24': 10777,
+ 'cat25': 10778,
+ 'cat26': 10779,
+ 'cat27': 10780,
+ 'cat28': 10781,
+ 'cat29': 10782,
+ 'cat30': 10783,
+ 'cat31': 10784,
+ 'cat32': 10785,
+ 'cat33': 10786,
+ 'cat34': 10787,
+ 'cat35': 10788,
+ 'cat36': 10789,
+ 'cat37': 10790,
+ 'cat38': 10791,
+ 'cat39': 10792,
+ 'cat40': 10793,
+ 'cat41': 10794,
+ 'cat42': 10795,
+ 'cat43': 10796,
+ 'cat44': 10797,
+ 'cat45': 10798,
+ 'cat46': 10799,
+ 'cat47': 10800,
+ 'cat48': 10801,
+ 'cat49': 10802,
+ 'cat50': 10803,
+ 'cat51': 10804,
+ 'cat52': 10805,
+ 'cat53': 10806,
+ 'cat54': 10807,
+ 'cat55': 10808,
+ 'cat56': 10809,
+ 'cat80': 20203,
+ 'cat81': 20204,
+ 'cat82': 20205,
+ 'cat88': 20209,
+ 'cat89': 20210,
+ 'cat90': 20211,
+ 'cat95': 20214,
+ 'cat96': 20215,
+ 'cat106': 20221,
+ 'cat116': 20227,
+ 'cat117': 20228,
+ 'cat123': 20232,
+ 'cat124': 20224
+}
+
+MEID_TO_CATEGORY_GBD_2019 = {
+ 10755: 'cat2',
+ 10761: 'cat8',
+ 10763: 'cat10',
+ 10764: 'cat11',
+ 10767: 'cat14',
+ 10768: 'cat15',
+ 10770: 'cat17',
+ 10772: 'cat19',
+ 10773: 'cat20',
+ 10774: 'cat21',
+ 10775: 'cat22',
+ 10776: 'cat23',
+ 10777: 'cat24',
+ 10778: 'cat25',
+ 10779: 'cat26',
+ 10780: 'cat27',
+ 10781: 'cat28',
+ 10782: 'cat29',
+ 10783: 'cat30',
+ 10784: 'cat31',
+ 10785: 'cat32',
+ 10786: 'cat33',
+ 10787: 'cat34',
+ 10788: 'cat35',
+ 10789: 'cat36',
+ 10790: 'cat37',
+ 10791: 'cat38',
+ 10792: 'cat39',
+ 10793: 'cat40',
+ 10794: 'cat41',
+ 10795: 'cat42',
+ 10796: 'cat43',
+ 10797: 'cat44',
+ 10798: 'cat45',
+ 10799: 'cat46',
+ 10800: 'cat47',
+ 10801: 'cat48',
+ 10802: 'cat49',
+ 10803: 'cat50',
+ 10804: 'cat51',
+ 10805: 'cat52',
+ 10806: 'cat53',
+ 10807: 'cat54',
+ 10808: 'cat55',
+ 10809: 'cat56',
+ 20203: 'cat80',
+ 20204: 'cat81',
+ 20205: 'cat82',
+ 20209: 'cat88',
+ 20210: 'cat89',
+ 20211: 'cat90',
+ 20214: 'cat95',
+ 20215: 'cat96',
+ 20221: 'cat106',
+ 20224: 'cat124',
+ 20227: 'cat116',
+ 20228: 'cat117',
+ 20232: 'cat123'
+}
+
+def read_lbwsg_data_by_draw_from_gbd2017_artifact(artifact_path, measure, draw, rename=None):
     """
     Reads one draw of LBWSG data from an artifact.
     
@@ -34,7 +177,7 @@ def read_lbwsg_data_by_draw(artifact_path, measure, draw, rename=None):
     data = pd.concat([index, draw], axis=1)
     return data
 
-def read_lbwsg_data1(artifact_path, measure, *filter_terms, draws='all'):
+def read_lbwsg_data_from_gbd2017_artifact1(artifact_path, measure, *filter_terms, draws='all'):
     query_string = ' and '.join(filter_terms)
     if draws=='all':
         draws = range(1000)
@@ -53,7 +196,7 @@ def read_lbwsg_data1(artifact_path, measure, *filter_terms, draws='all'):
 #     return data
     return pd.concat(draw_data_dfs, axis=1, copy=False)
 
-def read_lbwsg_data(artifact_path, measure, *filter_terms, draws='all'):
+def read_lbwsg_data_from_gbd2017_artifact(artifact_path, measure, *filter_terms, draws=None):
     """
     Reads multiple draws from the artifact.
     """
@@ -62,7 +205,7 @@ def read_lbwsg_data(artifact_path, measure, *filter_terms, draws='all'):
     # NOTE: If draws is a numpy array, this line throws a warning:
     #  "FutureWarning: elementwise comparison failed; returning scalar instead,
     #   but in the future will perform elementwise comparison"
-    if draws=='all':
+    if draws==None:
         draws = range(1000)
     
     with pd.HDFStore(artifact_path, mode='r') as store:
@@ -112,7 +255,7 @@ def get_lbwsg_categories_by_interval(include_missing=True):
     """
     category_dict = gbd.risk_factors.low_birth_weight_and_short_gestation.categories.to_dict()
     if include_missing:
-        category_dict.update(MISSING_CATEGORY)
+        category_dict.update(MISSING_CATEGORY_GBD_2017)
     cats = (pd.DataFrame.from_dict(category_dict, orient='index')
             .reset_index()
             .rename(columns={'index': 'cat', 0: 'name'}))
@@ -122,15 +265,16 @@ def get_lbwsg_categories_by_interval(include_missing=True):
     cats.index = idx
     return cats
 
-def get_ga_bw_by_category(include_missing=True):
+def get_ga_bw_by_category(include_missing=False):
     """
     Returns a dataframe indexed by LBWSG category, with four columns describing the intervals for that category:
     ga_start, ga_end, bw_start, bw_end
     """
     cats = gbd.risk_factors.low_birth_weight_and_short_gestation.categories.to_dict()
     if include_missing:
-        cats.update(MISSING_CATEGORY)
+        cats.update(MISSING_CATEGORY_GBD_2017)
     cats = pd.Series(cats)
+
     # Example string to extract from: 'Birth prevalence - [0, 24) wks, [0, 500) g'
     # extraction_regex = r'.*\[(?P<ga_start>\d+), (?P<ga_end>\d+).*\[(?P<bw_start>\d+), (?P<bw_end>\d+).*' # this also works
     extraction_regex = r'Birth prevalence - \[(?P<ga_start>\d+), (?P<ga_end>\d+)\) wks, \[(?P<bw_start>\d+), (?P<bw_end>\d+)\) g'
@@ -156,6 +300,33 @@ def get_category_data_by_interval(include_missing=False):
     cat_df['bw_width'] = cat_df['bw_end'] - cat_df['bw_start']
     
     return cat_df.set_index(['ga','bw'])
+
+def get_category_descriptions(source='gbd_mapping'):
+    if source=='gbd_mapping':
+        cats = gbd.risk_factors.low_birth_weight_and_short_gestation.categories.to_dict()
+        cats = (pd.Series(CATEGORY_TO_MEID_GBD_2019, name='modelable_entity_id')
+                .rename_axis('category')
+                .to_frame()
+                .join(pd.Series(cats, name='modelable_entity_name'))
+               )
+#         cats = pd.DataFrame.from_dict(cats, orient='index', columns=['modelable_entity_name']).rename_axis('category')
+    elif source=='get_ids':
+#         meid_query_string = f"modelable_entity_id in {CATEGORY_TO_MEID_GBD_2019.values()}"
+#         cats = db_queries.get_ids('modelable_entity').query(meid_query_string)
+#         cats = cats.reset_index(drop=True)
+        cats = (pd.DataFrame.from_dict(CATEGORY_TO_MEID_GBD_2019, orient='index', columns=['modelable_entity_id'])
+                .rename_axis('category')
+                .reset_index()
+                .merge(db_queries.get_ids('modelable_entity'))
+                .set_index('category')
+               )
+    else:
+        raise ValueError(f"Unknown source: {source}")
+
+    return cats
+
+def get_category_data(source='gbd_mapping', include_missing=False):
+    pass
 
 class LBWSGDistribution:
     """
