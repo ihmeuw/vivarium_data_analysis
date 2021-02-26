ec369258cbd2b458ebca926f5f3226218a4ad459
Nathaniel Blair-Stahn <ndbs@uw.edu>
2021-02-26 13:56:27 -0800

    add remaining age groups to get_age_group_data

--- pre_processing/lbwsg/demography.py
+++ pre_processing/lbwsg/demography.py
@@ -19,13 +19,15 @@ def get_age_group_data(birth_age_end=None, birth_age_end_description=None):
         (birth_age_end, birth_age_end_description),
         *((np.round(d/365, 8), f"{d} days = {d}/365 years, rounded to 8 decimals") for d in (7,28)),
         (1, "1 year"),
-        *((n, f"{n} years") for n in range(5,85,5)),
+        *((n, f"{n} years") for n in range(5,96,5)),
+        (np.inf, "infinity!"),
     ]
     # Unzip the list of 2-tuples to get two lists
     age_breaks, age_descriptions = zip(*age_breaks_and_descriptions)
     
     # Get age group names for the age group id's corresponding to the intervals between the age breaks
-    age_group_ids = [164, *range(2,21)]
+    # Birth, ENN, LNN, PNN, 1-4, 5-9,...,75-79, 80-85,...,90-94, 95+
+    age_group_ids = [164, *range(2,21), *range(30,33), 235]
     age_group_df = (get_ids('age_group')
                     .set_index('age_group_id')
                     .loc[age_group_ids]
@@ -39,32 +41,3 @@ def get_age_group_data(birth_age_end=None, birth_age_end_description=None):
     age_group_df['age_start_description'] = age_descriptions[:-1]
     age_group_df['age_end_description'] = age_descriptions[1:]
     return age_group_df
-
-#     age_intervals = pd.IntervalIndex.from_breaks(age_breaks, closed='left', name='age_group_interval')
-#     age_group_df = pd.DataFrame(
-#         {'age_group_id': age_group_ids,
-#          'age_group_name': 
-#          'age_group_start': age_breaks[:-1],
-#          'age_group_end': age_breaks[1:],
-#          'age_end_description': desc
-    
-#     age_breaks = [0, *np.round([birth_age_end, 7/365, 28/365], 8), 1, *range(5,85,5)]
-#     age_break_descriptions = ['0 days = 0 years',
-#                               birth_age_end_description,
-#                               '7 days = 7/365 years, rounded to 8 decimals',
-#                               '28 days = 28/365 years, rounded to 8 decimals',
-#                               '1 year',
-#                               *[f"{n} years" for n in range(5,85,5)],
-#                              ]
-#     age_intervals = pd.IntervalIndex.from_breaks(
-        
-#     birth_end = 5/(365*24*60) # five minutes
-#     seven_days = 7/365
-#     twenty_eight_days = 28/365
-#     breaks_under_one = np.round([birth_end, seven_days, twenty_eight_days], 8)
-#     age_intervals = pd.IntervalIndex.from_breaks(
-#         [0, *breaks_under_one, 1, *range(5,85,5)], closed='left', name='age_group_interval'
-#     )
-    
-#     age_group_ids = pd.Series([164, *range(2,21)], index=age_intervals, name='age_group_id')
-#     return age_group_ids
